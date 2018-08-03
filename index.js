995642033442240df14c1c11848a64fecf07dd9e
Leonardo Iacovini <leoiacovini@gmail.com>
2018-08-03 20:06:30 -0300

    Add routes for scripts run

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const simpleGit = require('simple-git');
 const path = require("path");
+const { promisify } = require('util');
+const { execFile } = require('child_process');
 const app = express();
+const execFileP = promisify(execFile);
 
 const PORT = process.env['STINGER_PORT'] || 3000;
 const REMOTE = process.env['GIT_REMOTE'] || 'tanajura';
 const APP_PATH = process.env['APP_PATH'] || '/app';
+const SCRIPTS_PATH = process.env['STINGER_SCRIPTS'] || '/scripts';
 
 const pullRepo = () => {
   try {
@@ -18,11 +22,53 @@ const pullRepo = () => {
   }
 }
 
-app.get('/pull', async (req, res) => {
+const restartProcess = async () => {
+  try {
+    await execFileP(path.resolve(SCRIPTS_PATH, 'restart.sh'));
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
+const stopProcess = async () => {
+  try {
+    await execFileP(path.resolve(SCRIPTS_PATH, 'stop.sh'));
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
+const startProcess = async () => {
+  try {
+    await execFileP(path.resolve(SCRIPTS_PATH, 'start.sh'));
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+}
+
+app.post('/pull', async (req, res) => {
   res.json({ ok: pullRepo() });
+});
+
+app.post('/restart', async (req, res) => {
+  res.json({ ok: await restartProcess() });
+});
+
+app.post('/stop', async (req, res) => {
+  res.json({ ok: await stopProcess() });
+});
+
+app.post('/start', async (req, res) => {
+  res.json({ ok: await startProcess() });
 });
 
 app.listen(PORT, function () {
   console.log(`Stinger is listening on port: ${PORT}`);
-  console.log(`APP_PATH: ${APP_PATH} - REMOTE: ${REMOTE}`)
+  console.log(`APP_PATH: ${APP_PATH}\nREMOTE: ${REMOTE}\nSCRIPTS: ${SCRIPTS_PATH}`)
 });
