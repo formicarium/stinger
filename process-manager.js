60a5b5c5a85902c513051c9ca8b4d8dd9bbb3bf2
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-08-07 02:16:17 -0300

    logs on process manager

--- process-manager.js
+++ process-manager.js
@@ -16,20 +16,32 @@ class ProcessManager {
   }
 
   async startProcess(path, args) {
-    const childProcess = spawn(path, args, {
-      stdio: [process.stdin, process.stdout, process.stderr]
-    })
-    this.registerProcess(childProcess)
-    return childProcess
+    console.log('Starting process...')
+    try {
+      const childProcess = spawn(path, args, {
+        stdio: [process.stdin, process.stdout, process.stderr]
+      })
+      this.registerProcess(childProcess)
+      console.log('Process started!')
+      return true
+    } catch (err) {
+      console.log('No process was started')
+
+      return false
+    }
+    
   }
 
   async killProcess() {
+    console.log('Trying to kill process...')
     const process = this.getProcess()
     if (process) {
       await kill(process.pid)
       this.process = null
+      console.log('Process was killed')
       return true
     }
+    console.log('No process was killed')
     return false
   }
 }
