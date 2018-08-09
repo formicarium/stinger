a96c3c482b538410baa985fac1cf714875c016f9
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-08-09 00:50:05 -0300

    using system kill

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,8 @@
-cd /tmp/remote/svc1
-lein run-dev
+# cd /tmp/remote/svc1
+# lein run-dev
 
-# echo "hello from script"
-# while [ 1 ]; do
-#   echo "alive";
-#   sleep 2;
-# done
+echo "hello from script"
+while [ 1 ]; do
+  echo "alive";
+  sleep 2;
+done
