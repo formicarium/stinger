64716af3355fa5a88b64212665122380dfb5e9ed
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-08-10 23:29:23 -0300

    using log

--- git-manager.js
+++ git-manager.js
@@ -1,6 +1,7 @@
 const del = require('del');
 const fs = require('fs-extra')
 const simpleGit = require('simple-git/promise')
+const { log } = require('./common')
 
 class GitManager {
   constructor(baseFolder, remoteUrl) {
@@ -9,25 +10,25 @@ class GitManager {
   }
 
   async clone() {
-    console.log('Trying to clone repo')
+    log('Trying to clone repo')
     const repo = await simpleGit();
     const clone = await repo.clone(this.remoteUrl, this.baseFolder, ['-b', 'tanajura']);
-    console.log('Repo was cloned')
+    log('Repo was cloned')
     return clone
   }
 
   async pull() {
-    console.log('Pulling repo...')
+    log('Pulling repo...')
     const repo = await simpleGit(this.baseFolder);
     const pull = await repo.pull('origin', 'tanajura')
-    console.log('Pull success')  
+    log('Pull success')  
     return pull
   }
 
   async delete() {
-    console.log('Delete existing')
+    log('Delete existing')
     const delResponse = await del(this.baseFolder, { force: true })
-    console.log('Deleted')
+    log('Deleted')
     return delResponse
   }
 
